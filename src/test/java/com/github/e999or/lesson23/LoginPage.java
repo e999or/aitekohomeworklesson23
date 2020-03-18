13dccbd17a55a822f94a8f9dda916867c51696b3
Lenovo-PC <e999or@bk.ru>
2020-03-18 23:52:06 +0300

    allure work

--- src/test/java/com/github/e999or/lesson23/LoginPage.java
+++ src/test/java/com/github/e999or/lesson23/LoginPage.java
@@ -1,22 +1,23 @@
 package com.github.e999or.lesson23;
 
 import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage {
-    @Step("Открыть ссылку")
+    @Step("Открыть ссылку https://idemo.bspb.ru")
     LoginPage open() {
         Selenide.open("https://idemo.bspb.ru");
         return this;
     }
-
+    @Step("Кликнуть кнопку \"Войти\"")
     LoginPage clickLoginButton(){
         $(By.id("login-button")).click();
         return this;
     }
-
+    @Step("Проверить, находимся ли мы на странице 2х факторной авторизации")
     boolean inspectCodePhone(){
         String phone = $(By.xpath("//div[@id='otp-code-text']/b")).getAttribute("innerText");
         if(phone.equals("*1111")) {
@@ -26,12 +27,12 @@ public class LoginPage {
         }
     }
 
-
+    @Step("В поле ввода ввести одноразовый пароль")
     LoginPage sendKeyskOtpCode(String keys){
         $(By.id("otp-code")).sendKeys(keys);
         return this;
     }
-
+    @Step("Кликнуть кнопку \"Войти\"")
     LoginPage clikOtpCode(){
         $(By.id("login-otp-button")).click();
         return this;
--- src/test/java/com/github/e999or/lesson23/ReviewButton.java
+++ src/test/java/com/github/e999or/lesson23/ReviewButton.java
@@ -1,5 +1,6 @@
 package com.github.e999or.lesson23;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 
 import java.util.regex.Matcher;
@@ -9,14 +10,15 @@ import static com.codeborne.selenide.Selenide.$;
 
 
 public class ReviewButton {
-
+    @Step("Кликнуть на вкладку \"Обзор\"")
     void clikIdButton(){
         $ (By.id("bank-overview")).click();
     }
+    @Step("Кликнуть на поле \"Финансовая свобода\", которое отображает денежные стредства")
     void clikxPathButton(){
        $ (By.xpath("//small[@class='my-assets']")).click();
     }
-
+    @Step("Проверить, появляется ли в поле,\"Финансовая свобода\", которое отображает денежные стредства, надпись:\"Финансовая свобода\"")
     boolean inspectTitle(){
         String text = $(By.id("can-spend")).getAttribute("innerText");
         if((text).contains( "Финансовая свобода")) {
@@ -25,7 +27,7 @@ public class ReviewButton {
             return false;
         }
     }
-
+    @Step("Проверить поле,\"Финансовая свобода\", которое отображает денежные стредства, соответствует ли формату 123 567 256.00 \u20BD")
     boolean inspectTipe(){
         String str = $(By.xpath("//small[@class='my-assets']")).getAttribute("innerText");
         String sy = String.valueOf(new StringBuffer(str).delete(0,14));
